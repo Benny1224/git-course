059167f2b32d9ff5d2004a07760369c82ffc08a3
bye-code <benny_goer@outlook.es>
2024-07-12 17:15:41 -0600

    Estructura:creando un proyecto básico en Flask. BC

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from src.app import sum
-def main () -> None:
-    sum (5,10)
-    if __name__ == '__main__':
-        main ()
+from src.app import app
+
+HOST="localhost"
+PORT=4000
+DEBUG=True
+
+if __name__ == '__main__':
+        app.run(HOST, PORT, DEBUG)
